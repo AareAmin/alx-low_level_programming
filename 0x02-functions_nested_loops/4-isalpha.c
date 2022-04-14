d9946ad30238f51b579f1ffb8ee375024738777f
AareAmin <aarelatosa@gmail.com>
2022-04-14 00:59:46 -0700

    task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,12 +3,12 @@
 /**
  * _isalpha - tests whether a character is from the English alphabet.
  * @c : character to be checked
- * Return: 1 if character is an English character 0 if it is not an English character
+ * Return: 1 if character is an English character 
+ * 0 if it is not an English character
  */
 int _isalpha(int c)
 {
-if
-((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+if (((c >= 'a' && c <= 'z')) || ((c >= 'A' && c <= 'Z')))
 return (1);
 else
 return (0);
