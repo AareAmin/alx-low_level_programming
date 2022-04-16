f5faa8e33bd468da8b6f0388f7f5f392bb879f38
AareAmin <aarelatosa@gmail.com>
2022-04-16 00:37:30 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - returns the sum of its parameters
+ * @a: int type number
+ * @b: int type number
+ * Return:0 or 1
+ */
+void print_to_98(int n)
+{
+return (a + b);
+}
